cbf96e98e156b582eea273ce41dda033bb0858cf
Robin Eriksson <robin.eriksson2@learnet.se>
2024-11-19 11:08:29 +0100

    First

---
+++ server.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+dotenv.config()
+import express from 'express'
+import pool  from './db.js'
+
+const app = express()
+const port = 3000
+
+app.get('/', async (req, res) => {
+  const [birds] = await pool.promise().query('SELECT * FROM birds')
+
+  res.json(birds)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
