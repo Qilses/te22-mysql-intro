48d4aa93ff2781ae2a3a8429d95a849c0972ee07
Robin Eriksson <robin.eriksson2@learnet.se>
2024-11-26 11:16:19 +0100

    First

--- server.js
+++ server.js
@@ -1,17 +1,55 @@
-import 'dotenv/config'
-dotenv.config()
-import express from 'express'
-import pool  from './db.js'
+import "dotenv/config"
+import express from "express"
+import pool from "./db.js"
+import nunjucks from 'nunjucks'
 
 const app = express()
 const port = 3000
 
-app.get('/', async (req, res) => {
-  const [birds] = await pool.promise().query('SELECT * FROM birds')
+nunjucks.configure('views', { //Detta ska bara vara   
+  autoescape: true,
+  express: app,
+})
 
+app.get("/birds", async (req, res) => {
+  // const [birds] = await pool.promise().query('SELECT * FROM birds')
+ 
+  const [birds] = await pool
+    .promise()
+    .query(
+      `SELECT birds.*,
+      spieces.name AS spieces FROM birds 
+      JOIN spieces ON birds.species_id = spieces.id;`
+
+    )
+    
   res.json(birds)
+})  
+
+app.get("/birds/:id", async (req, res) => {
+  const [bird] = await pool
+    .promise()
+    .query(
+      `SELECT birds.*, 
+      spieces.name AS spieces FROM birds 
+      JOIN spieces ON birds.species_id = spieces.id WHERE birds.id = ?;`,
+      [req.params.id],
+    )
+    res.render("birds.njk", {
+      title: 'bird',
+      bird: bird[0],
+      
+    }) 
+  //res.json(bird[0])
+ 
+})
+
+app.use((req, res) => {
+  res.status(404).render('404.njk', {
+    title: '404  not found',
+  })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
