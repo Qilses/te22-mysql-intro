aef487d55912be3f3123df4ace7ec37312af7344
Robin Eriksson <robin.eriksson2@learnet.se>
2024-12-03 11:22:45 +0100

    Update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import "dotenv/config"
 import express from "express"
+import nunjucks from "nunjucks"
+import morgan from "morgan"
+import bodyParser from "body-parser"
+
 import pool from "./db.js"
-import nunjucks from 'nunjucks'
+import birdsRouter from "./routes/birds.js"
+
 
 const app = express()
 const port = 3000
@@ -11,6 +16,20 @@ nunjucks.configure('views', { //Detta ska bara vara
   express: app,
 })
 
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"))
+
+app.get("/", async (req, res) => {
+  res.render("index.njk", {
+    title: "Hello World",
+    message: "Hello World",
+  })
+})
+
+app.use("/birds", birdsRouter)
 app.get("/birds", async (req, res) => {
   // const [birds] = await pool.promise().query('SELECT * FROM birds')
  
